abc943aad740860242658a55361f871ca2de0b79
Vishal Munagekar <vishal.munagekar@tietoevry.com>
2024-06-19 11:25:27 +0530

    completed interview question

--- untitled01/src/com/interview/example01.java
+++ untitled01/src/com/interview/example01.java
@@ -1,32 +1,33 @@
 package com.interview;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class example01 {
 
     public static void main(String[] args) {
         List<Card> cardList = new ArrayList<>();
         String heart = "HEART";
-        String dimond = "DIMOND";
-        String club = "CLUD";
+        String diamond = "DIAMOND";
+        String club = "CLUB";
         String spade = "SPADE";
 
-        int num = new Random(0).nextInt();
-        System.out.println(num);
-
-//        for (int i = 0; i < 13; i++){
-//            String value = Integer.valueOf(i).toString();
-//            if(i == 11) value = "JOKER";
-//            if(i == 12) value = "QUEEN";
-//            if(i == 13) value = "JOKER";
-//
-//            cardList.add(new Card(heart, ));
-//            cardList.add(new Card(dimond, Integer.valueOf(i).toString()));
-//            cardList.add(new Card(club, Integer.valueOf(i).toString()));
-//            cardList.add(new Card(spade, Integer.valueOf(i).toString()));
-//        }
+        for (int i = 1; i <= 13; i++){
+            String value = Integer.valueOf(i).toString();
+            if(i == 1) value = "A";
+            if(i == 11) value = "JOKER";
+            if(i == 12) value = "QUEEN";
+            if(i == 13) value = "KING";
+            cardList.add(new Card(heart, value));
+            cardList.add(new Card(diamond, value));
+            cardList.add(new Card(club, value));
+            cardList.add(new Card(spade, value));
+        }
+        cardList.stream().forEach(System.out::println);
+        Collections.shuffle(cardList);
+        System.out.println("-------------------- shuffled --------------------");
+        cardList.stream().forEach(System.out::println);
     }
 }
 class Card {
@@ -37,6 +38,8 @@ class Card {
         this.face = face;
         this.value = value;
     }
-
-
+    @Override
+    public String toString() {
+        return "Card{" + "face='" + face + '\'' + ", value='" + value + '\'' + '}';
+    }
 }
